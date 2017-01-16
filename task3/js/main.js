5fed28a279b2d384d808ab38002cb20acde8ea35
Дмитрий <dmitrom89@gmail.com>
2017-01-16 09:01:47 +0200

    edit navigation class active

--- task3/js/main.js
+++ task3/js/main.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
    $('.name, .avatar').click(function () {
-
        if($('.info').css('display') == 'none') {
            $('.info').show();
        } else {
            $('.info').hide();
        }
    });
+
+    
 
 });
