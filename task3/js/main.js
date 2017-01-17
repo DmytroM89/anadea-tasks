6a1ea4340afa1e738f2b2716fc2dea65af18010d
unknown <MartynenkoDV@MartynenkoDV.aku.com>
2017-01-17 18:03:45 +0200

    show/hide block '.info', edit block '.info'

--- task3/js/main.js
+++ task3/js/main.js
@@ -1,18 +1,23 @@
 $(document).ready(function () {
-   $('.name, .avatar').click(function () {
-       if($('.info').css('display') == 'none') {
-           $('.info').show();
-       } else {
-           $('.info').hide();
-       }
-   });
 
-$(document).mouseup(function (e) {
-    var blockInfo = $('.info');
-    if (blockInfo.has(e.target).length === 0){
-        blockInfo.hide();
-    }
-});
+    // $('.toggle-info').on('click', function () {
+    //    var blockInfo = $(this).parents('.message').children('.info');
+    //    var style = $(blockInfo).css('display');
+    //    console.log(style);
+    //
+    //    if($(blockInfo).css('display') == 'none') {
+    //        $(blockInfo).show();
+    //    } else {
+    //        $(blockInfo).hide();
+    //    }
+    // });
+    //
+    // $(document).mouseup(function (e) {
+    //     var blockInfo = $('.info');
+    //     if (blockInfo.has(e.target).length === 0){
+    //         blockInfo.hide();
+    //     }
+    // });
     
 
 });
