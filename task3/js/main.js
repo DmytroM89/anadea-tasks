f587fac48b5040907b1702fe20cc1ffe04d26962
Дмитрий <dmitrom89@gmail.com>
2017-01-18 01:34:16 +0200

    finish 'Overview' block

--- task3/js/main.js
+++ task3/js/main.js
@@ -1,23 +1,23 @@
 $(document).ready(function () {
 
-    // $('.toggle-info').on('click', function () {
-    //    var blockInfo = $(this).parents('.message').children('.info');
-    //    var style = $(blockInfo).css('display');
-    //    console.log(style);
-    //
-    //    if($(blockInfo).css('display') == 'none') {
-    //        $(blockInfo).show();
-    //    } else {
-    //        $(blockInfo).hide();
-    //    }
-    // });
-    //
-    // $(document).mouseup(function (e) {
-    //     var blockInfo = $('.info');
-    //     if (blockInfo.has(e.target).length === 0){
-    //         blockInfo.hide();
-    //     }
-    // });
+    $('.toggle-info').on('click', function () {
+       var blockInfo = $(this).parents('.message').children('.info');
+       var style = $(blockInfo).css('display');
+       console.log(style);
+
+       if($(blockInfo).css('display') == 'none') {
+           $(blockInfo).show();
+       } else {
+           $(blockInfo).hide();
+       }
+    });
+
+    $(document).mouseup(function (e) {
+        var blockInfo = $('.info');
+        if (blockInfo.has(e.target).length === 0){
+            blockInfo.hide();
+        }
+    });
     
 
 });
