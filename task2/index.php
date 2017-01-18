1349088aeddf8c77002ddfc76c8e6994a44c91f7
Dmitro Martynenko <dmitrom89@gmail.com>
2017-01-18 16:45:22 +0200

    correction page-proofs

--- task2/index.php
+++ task2/index.php
@@ -9,23 +9,59 @@
     <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-   <div class="wrapper">
-    <!-- Header/Navbar  -->
-    <nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="#">Kohl Corp. Vacation planning</a>
-            </div>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="">Hi, Bruce</a></li>
-                <li><a href=""><span class="icon-mail"></span>2 new</a></li>
-                <span class="v-line"></span>
-                <li><a href=""><span class="icon-eject"></span>Logout</a></li>
-            </ul>
-        </div>
-    </nav>
 
-   </div>
+    <div class="wrapper">
+        <!-- Header/Navbar  -->
+        <nav class="navbar navbar-default navbar-static-top">
+            <div class="container">
+                <div class="navbar-header"> <a class="navbar-brand" href="#">Kohl Corp. Vacation planning</a> </div>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="">Hi, Bruce</a></li>
+                    <li><a href=""><span class="icon-mail"></span>2 new</a></li> <span class="v-line"></span>
+                    <li><a href=""><span class="icon-eject"></span>Logout</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-9 text-block-wrapper">
+                    <div class="panel panel-default text-block">
+                        <div class="panel-heading text-head">
+                            <h3 class="panel-title">Почему комулятивно мелькание мыслей?</h3> </div>
+                        <div class="panel-body text-body">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc malesuada facilisis nunc, vitae iaculis risus. Pellentesque efficitur turpis ut mollis imperdiet. Morbi vel tincidunt ante, et cursus augue. Donec scelerisque, erat eget eleifend pulvinar, ligula sem aliquet arcu, sollicitudin finibus leo ligula eu est. Pellentesque quis ligula quis diam vehicula interdum. Maecenas sed mi consequat, ultricies turpis id, tristique elit. Vivamus elit risus, egestas vitae volutpat sit amet, hendrerit ut lorem. Pellentesque aliquet volutpat elit vel maximus. Suspendisse non lorem dolor. In hac habitasse platea dictumst. Nullam facilisis tellus ligula, non maximus tellus pellentesque at. Mauris egestas eu leo ac bibendum. Pellentesque ut leo odio. Aenean in metus vel purus malesuada finibus sed ut ipsum.</p>
+                            <p>Praesent pellentesque mauris enim, a viverra sapien facilisis ut. Donec urna sapien, posuere ut semper sed, dictum ut lacus. Quisque sodales dolor metus, sit amet pulvinar nibh efficitur ut. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus lacinia at risus vel consequat. Suspendisse vel ligula dolor. Mauris semper risus quis velit gravida mollis. Nam ornare erat quis mauris rhoncus, id aliquet tortor ornare. Maecenas pharetra auctor mi, aliquam cursus nunc aliquet sed. In luctus turpis dolor, eu fermentum erat lobortis vel. Praesent luctus commodo massa, ac sagittis erat accumsan non. Phasellus arcu enim, hendrerit vitae elit ac, vehicula faucibus urna. Phasellus commodo eros at orci tempor, eget dignissim dui suscipit. Sed eget mattis massa, non vulputate mauris.</p>
+                            <p>Cras sit amet neque arcu. Nulla mattis massa vitae lectus tincidunt, a rhoncus nulla placerat. Nullam vel lacus et leo sodales porta. Morbi aliquam eu nunc eget pretium. Sed lacinia neque ligula, a vulputate est bibendum ultricies. Donec faucibus commodo augue, at semper urna laoreet at. Pellentesque hendrerit ultricies vulputate. Quisque facilisis purus in accumsan faucibus. Morbi semper tincidunt sollicitudin. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent feugiat sem id metus lacinia condimentum.</p>
+                        </div>
+                    </div>
+                    <div class="panel panel-default text-block">
+                        <div class="panel-heading text-head">
+                            <h3 class="panel-title">Почему комулятивно мелькание мыслей?</h3> </div>
+                        <div class="panel-body text-body">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc malesuada facilisis nunc, vitae iaculis risus. Pellentesque efficitur turpis ut mollis imperdiet. Morbi vel tincidunt ante, et cursus augue. Donec scelerisque, erat eget eleifend pulvinar, ligula sem aliquet arcu, sollicitudin finibus leo ligula eu est. Pellentesque quis ligula quis diam vehicula interdum. Maecenas sed mi consequat, ultricies turpis id, tristique elit. Vivamus elit risus, egestas vitae volutpat sit amet, hendrerit ut lorem. Pellentesque aliquet volutpat elit vel maximus. Suspendisse non lorem dolor. In hac habitasse platea dictumst. Nullam facilisis tellus ligula, non maximus tellus pellentesque at. Mauris egestas eu leo ac bibendum. Pellentesque ut leo odio. Aenean in metus vel purus malesuada finibus sed ut ipsum.</p>
+                            <p>Praesent pellentesque mauris enim, a viverra sapien facilisis ut. Donec urna sapien, posuere ut semper sed, dictum ut lacus. Quisque sodales dolor metus, sit amet pulvinar nibh efficitur ut. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus lacinia at risus vel consequat. Suspendisse vel ligula dolor. Mauris semper risus quis velit gravida mollis. Nam ornare erat quis mauris rhoncus, id aliquet tortor ornare. Maecenas pharetra auctor mi, aliquam cursus nunc aliquet sed. In luctus turpis dolor, eu fermentum erat lobortis vel. Praesent luctus commodo massa, ac sagittis erat accumsan non. Phasellus arcu enim, hendrerit vitae elit ac, vehicula faucibus urna. Phasellus commodo eros at orci tempor, eget dignissim dui suscipit. Sed eget mattis massa, non vulputate mauris.</p>
+                            <p>Cras sit amet neque arcu. Nulla mattis massa vitae lectus tincidunt, a rhoncus nulla placerat. Nullam vel lacus et leo sodales porta. Morbi aliquam eu nunc eget pretium. Sed lacinia neque ligula, a vulputate est bibendum ultricies. Donec faucibus commodo augue, at semper urna laoreet at. Pellentesque hendrerit ultricies vulputate. Quisque facilisis purus in accumsan faucibus. Morbi semper tincidunt sollicitudin. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent feugiat sem id metus lacinia condimentum.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 sidebar-wrapper">
+                    <div class="nav nav-sidebar sidebar affix">
+                        <ul class="nav">
+                            <li><a href="">Company</a></li>
+                            <li><a href="">Modules</a></li>
+                            <li><a href="">Time off</a></li>
+                            <li><a href="">Purchases</a></li>
+                            <li><a href="">Exprenses</a></li>
+                            <li><a href="">Appreisals</a></li>
+                            <li><a href="">Plans and Payment</a></li>
+                            <li><a href="">Handbook</a></li>
+                            <li><a href="">Change company owner</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 </body>
