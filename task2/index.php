5b5236312baeb4d7afea00dae142e80f94098806
Dmitro Martynenko <dmitrom89@gmail.com>
2017-01-18 12:56:00 +0200

    edit task2

---
+++ task2/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task 2</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/_sprite.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+   <div class="wrapper">
+    <!-- Header/Navbar  -->
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">Kohl Corp. Vacation planning</a>
+            </div>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="">Hi, Bruce</a></li>
+                <li><a href=""><span class="icon-mail"></span>2 new</a></li>
+                <span class="v-line"></span>
+                <li><a href=""><span class="icon-eject"></span>Logout</a></li>
+            </ul>
+        </div>
+    </nav>
+
+   </div>
+
+
+</body>
+</html>
